b8e3ae21e3d8d91c7c049db9ce085b47cd3cf03c
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-11-28 14:20:48 -0500

    changes for railway

--- 75/src/main/java/com/tracker5/s3/S3Config.java
+++ 75/src/main/java/com/tracker5/s3/S3Config.java
@@ -8,13 +8,12 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-    @Value("us-east-1")
-    private String awsRegion;
+
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.of(awsRegion))
+                .region(Region.of("us-east-1"))
                 .build();
     }
 }
