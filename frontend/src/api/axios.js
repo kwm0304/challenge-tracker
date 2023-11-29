1326ffef38c65a8c08cbaba69ec35ce7b87f932b
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-11-29 10:59:13 -0500

    changed axiosurl back for prod

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const apiClient = axios.create(
   {
-    // baseURL: 'https://challenge-tracker-production.up.railway.app/'
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://challenge-tracker-production.up.railway.app/'
+    // baseURL: 'http://localhost:8080/'
   }
 )
