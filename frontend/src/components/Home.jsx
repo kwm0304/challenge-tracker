69ac29521623c3875f6585808cc88de1d84eb581
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-11-29 11:31:57 -0500

    changes to yml

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
 return (
   <div className="bg-gradient-to-b from-amber-300 via-amber-900 to-black">
   <div className="flex flex-col items-center justify-center min-h-screen">
-    <h1 className="font-display text-white text-6xl text-center">75 Day Challenge</h1>
+    <h1 className="font-display text-white text-6xl text-center mb-12">75 Day Challenge</h1>
   <div className="mt-12 text-2xl grid gap-y-8">
     <div className="">
     <button className="bg-green-600 w-48 py-3 rounded-xl text-white font-semibold">
