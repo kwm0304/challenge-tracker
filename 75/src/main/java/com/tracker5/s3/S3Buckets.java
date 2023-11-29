d442fcd3f6c19d447a9b6a94d13ce189f850fabf
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-11-29 11:55:51 -0500

    s3 changes for railway

--- 75/src/main/java/com/tracker5/s3/S3Buckets.java
+++ 75/src/main/java/com/tracker5/s3/S3Buckets.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "aws.s3.buckets")
 public class S3Buckets {
 
     private String checklist = "${bucketString}";
